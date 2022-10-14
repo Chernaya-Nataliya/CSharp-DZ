9227d985deb0b6c474a948b83ca689a43492597d
Nataliya <chernishmail@gmail.com>
2022-10-14 08:35:28 +0300

    Добавили DZ7

---
+++ DZ7/47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Write("Введите количество столбцов m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"m = {m}, n = {n}.");
+double[,] array = new double[m, n];
+ArrayDouble(array);
+GetArray(array);
+
+void ArrayDouble(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void GetArray (double[,] array)
+{
+for (int i = 0; i < m; i++)
+  {
+      Console.Write("[ ");
+      for (int j = 0; j < n; j++)
+      {
+        double Num = Math.Round(array[i, j], 1);
+        Console.Write(Num + " ");
+      }
+      Console.Write("]");
+      Console.WriteLine();
+  }
+}
---
+++ DZ7/50/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// (1, 7) -> такого числа в массиве нет
+Console.Write("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] Num = new int [5,10];
+FillArrayRandomNum(Num);
+
+if (n > Num.GetLength(0) || m > Num.GetLength(1))
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца = {Num[n-1,m-1]}");
+}
+
+PrintArray(Num);
+
+void FillArrayRandomNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
